5967c7e8a456b612972204bd10654c95fc637b0a
WOOSAH <31331596+Smiley-McSmiles@users.noreply.github.com>
2024-08-03 15:37:45 -0500

    Initial 0.1.0 Release!

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# llaman
+> LLaMan v0.1.0 - A Ollama and Open-WebUI manager written in BASH
+
+> Tested on Fedora 40
+
+> Should work on Any Debian, Arch, or RHEL Based Distribution **with SystemD**
+
+# Description
+
 LLaMan is a lightweight BASH CLI (Command Line Interface) tool for installing and managing Ollama and Open-WebUI.
+
+# Getting Started
+
+```sh
+git clone https://github.com/Smiley-McSmiles/llaman
+cd llaman
+chmod ug+x setup.sh
+sudo ./setup.sh
+cd ~/
+```
+
+# Features
+
+* **Setup** - Sets up the initial install.
+* **Update** - Downloads and updates to the latest Ollama or Open-WebUI version
+* **Disable** - Disable ollama.service and open-webui.service
+* **Enable** - Enable ollama.service and open-webui.service
+* **Start** - Start ollama.service and open-webui.service
+* **Stop** - Stop ollama.service and open-webui.service
+* **Restart** - Restart ollama.service and open-webui.service
+* **Status** - Get status of ollama.service and open-webui.service
+* **Download** - Download a .gguf file from Hugging Face via URL
+* **Remove** - Remove a model from Ollama
+* **Install** - Install a downloaded .gguf model.
+* **Backup** - Input a directroy to output a backup archive.
+* **Import** - Import a .tar file to pick up where you left off on another system.
+* **Get Version** - Get the current installed version of LLaMan, Ollama, and Open-WebUI.
+* **View Logs** - Select from a list of logs to view.
+* **Uninstall** - Uninstalls LLaMan, Ollama, and Open-WebUI completely
+
+# Usage
+
+llaman [PARAMETER]
+
+PARAMETERS:
+-b = Backup Open-WebUI users and settings
+-e = Enable Ollama and Open-WebUI
+-d = Disable Ollama and Open-WebUI
+-s = Start Ollama and Open-WebUI
+-S = Stop Ollama and Open-WebUI
+-r = Restart Ollama and Open-WebUI
+-i = Install a downloaded .gguf model
+-r = remove model from Ollama
+-D = download .gguf file from https://huggingface.co
+-u = update Ollama and Open-Webui
+-v = get LLaMan, Ollama, and Open-Webui version
+-h = display this help menu
+-X = Uninstall LLaMan, Ollama, and Open-Webui
+"Example: sudo llaman -d
+
+### License
+
+   This project is licensed under the [GPL V3.0 License](https://github.com/Smiley-McSmiles/llaman/blob/main/LICENSE).
+
---
+++ setup.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+DIRECTORY=$(cd `dirname $0` && pwd)
+httpPort=8080
+defaultUser=open-webui
+defaultDir=/opt/open-webui
+logFile=$defaultDir/log/llaman.log
+
+source $DIRECTORY/scripts/base_functions.sh
+
+InstallDependencies()
+{
+	packagesNeededRHEL="npm python3-devel make automake gcc gcc-c++ kernel-devel curl screen"
+	packagesNeededDebian="npm python3-dev make automake gcc g++ linux-headers-$(uname -r) screen"
+	packagesNeededArch="npm python-devtools make automake gcc linux-headers curl screen"
+	packagesNeededOpenSuse="npm python-devel python312-devel make automake gcc g++ kernel-devel curl screen"
+	echo "> Preparing to install needed dependancies for Jellyfin..."
+
+	if [[ -f /etc/os-release ]]; then
+		source /etc/os-release
+		crbOrPowertools=
+		osDetected=true
+		echo "> ID=$ID"
+		
+		if [[ $ID_LIKE == .*"rhel".* ]] || [[ $ID == "rhel" ]]; then
+			ID=rhel
+			
+			if [[ $VERSION_ID == *"."* ]]; then
+				VERSION_ID=$(echo $VERSION_ID | cut -d "." -f 1)
+			fi
+			
+			if (( $VERSION_ID < 9 )); then
+				crbOrPowertools="powertools"
+			else
+				crbOrPowertools="crb"
+			fi
+		fi
+		
+			case "$ID" in
+				fedora)	dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+								dnf install $packagesNeededRHEL -y ;;
+				rhel)			dnf install epel-release -y
+								dnf config-manager --set-enabled $crbOrPowertools
+								dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm -y https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm -y
+								dnf install $packagesNeededRHEL -y ;;
+				debian)			apt install $packagesNeededDebian -y ;;
+				ubuntu)			apt install $packagesNeededDebian -y ;;
+				linuxmint)		apt install $packagesNeededDebian -y ;;
+				elementary)		apt install $packagesNeededDebian -y ;;
+				arch)			pacman -Syu $packagesNeededArch ;;
+				endeavouros)	pacman -Syu $packagesNeededArch ;;
+				manjaro)		pacman -Syu $packagesNeededArch ;;
+				opensuse*)		zypper install $packagesNeededOpenSuse ;;
+			esac
+	else
+		osDetected=false
+		echo "+-------------------------------------------------------------------+"
+		echo "|                       ******WARNING******                         |"
+		echo "|                        ******ERROR******                          |"
+		echo "|               FAILED TO FIND /etc/os-release FILE.                |"
+		echo "+-------------------------------------------------------------------+"
+		
+		read -p "Press ENTER to continue" ENTER
+	fi
+}
+
+Setup()
+{
+	HasSudo
+	configFile=$defaultDir/config/llaman.conf
+	serviceLocation=
+	ollamaModelsDirectory=/usr/share/ollama/.ollama/models
+	
+	PromptUser dir "Please enter the storage directory for .gguf models" 0 0 "/path/to/directory"
+	ggufDirectory=$promptResult
+	
+	echo "Would you like a custom Ollama Model directory?"
+	if PromptUser yN "Default Ollama model directory is: /usr/share/ollama/.ollama/models" 0 0 "y/N"; then
+		PromptUser dir "Please enter the new storage directory for installed Ollama models" 0 0 "/path/to/directory"
+		ollamaModelsDirectory=$promptResult
+	fi
+
+	InstallDependencies
+
+	if [[ ! -d /opt/open-webui/config ]]; then
+		mkdir -p $defaultDir/config $defaultDir/log
+	fi
+	
+	SetVar configFile $configFile "$configFile" str
+	SetVar defaultUser $defaultUser "$configFile" str
+	SetVar defaultDir $defaultDir "$configFile" str
+	SetVar logFile $logFile "$configFile" str
+	SetVar httpPort $httpPort "$configFile" str
+	SetVar ggufDirectory $ggufDirectory "$configFile" str
+	SetVar modelFiles $defaultDir/config/modelfiles "$configFile" str
+	SetVar ollamaModelsDirectory $ollamaModelsDirectory "$configFile" str
+	
+	curl -fsSL https://ollama.com/install.sh | sh
+	
+	if [[ ! $ollamaModelsDirectory == /usr/share/ollama/.ollama/models ]]; then
+		rm -rf /usr/share/ollama/.ollama/models
+		ln -S $ollamaModelsDirectory /usr/share/ollama/.ollama/models
+		chown -Rf ollama:ollama /usr/share/ollama/.ollama/models
+		chmod -Rf 770 /usr/share/ollama/.ollama/models
+	fi
+	
+	echo "> Unblocking port $httpPort"
+	if [ -x "$(command -v ufw)" ]; then
+		Log "SETUP | Using ufw to unblock $httpPort" $logFile
+		ufw allow $httpPort/tcp
+		ufw reload
+	elif [ -x "$(command -v firewall-cmd)" ]; then
+		Log "SETUP | Using firewalld to unblock $httpPort" $logFile
+		firewall-cmd --permanent --add-port=$httpPort/tcp
+		firewall-cmd --reload
+	fi
+
+	cp -f $DIRECTORY/scripts/llaman /usr/bin/
+	cp -f $DIRECTORY/scripts/base_functions.sh /usr/bin/
+	cp -rf $DIRECTORY/configs/modelfiles $defaultDir/config/
+	chmod +x /usr/bin/llaman
+	chmod +x /usr/bin/base_functions.sh
+
+	if [ -d /usr/lib/systemd/system ]; then
+		serviceLocation="/usr/lib/systemd/system"
+		SetVar serviceLocation $serviceLocation "$configFile" str
+		cp -f $DIRECTORY/configs/*.service $serviceLocation/
+	else
+		serviceLocation="/etc/systemd/system"
+		SetVar serviceLocation $serviceLocation "$configFile" str
+	fi
+
+	cp -f $DIRECTORY/configs/*.service $serviceLocation/
+	SetVar User $defaultUser $serviceLocation/open-webui.service str
+
+	if id "$defaultUser" &>/dev/null; then
+		groupdel $defaultUser
+		userdel $defaultUser
+	fi
+
+	useradd -rd $defaultDir $defaultUser
+
+	cd $defaultDir
+	git clone https://github.com/open-webui/open-webui.git
+	cd open-webui/
+
+	cp -RPp .env.example .env
+
+	npm i
+	npm run build
+
+	cd backend/
+	pip install -r requirements.txt -U
+	
+	chown -Rf $defaultUser:$defaultUser $defaultDir
+	
+	sed -ri "s|exec uvicorn|python -m uvicorn|g" start.sh
+
+	chmod -Rf 770 $defaultDir
+	
+	systemctl enable --now ollama open-webui
+	
+	echo "> Ollama and Open-WebUI is now installed"
+	echo "> Please visit http://localhost:8080"
+}
+
+Setup
