54addf430581bd4b5a43f4041f6ed26db2f4dd5e
WOOSAH <31331596+Smiley-McSmiles@users.noreply.github.com>
2024-08-04 11:57:08 -0500

    Add files via upload

--- .github/version_change.sh
+++ .github/version_change.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-source ../scripts/llaman
-currentVersion=$llamanVersion
+currentVersion=$(head -n 10 ../llaman.1 | grep -o "[0-9].[0-9].[0-9]")
 echo "Current version is v$currentVersion"
 echo
 echo "Please enter the new version number"
 echo "EXAMPLE: $currentVersion"
 read newVersion
 
-sed -i -e "s|v$currentVersion|v$newVersion|g" ../README.md
-# sed -i -e "s|v$currentVersion|v$newVersion|g" ../jellyman.1
-sed -i -e "s|v$currentVersion|v$newVersion|g" ../scripts/llaman
-sed -i -e "s|v$currentVersion|v$newVersion|g" ../setup.sh
+sed -i "s|v$currentVersion|v$newVersion|g" ../README.md
+sed -i "s|v$currentVersion|v$newVersion|g" ../llaman.1
+sed -i "s|v$currentVersion|v$newVersion|g" ../scripts/llaman
+sed -i "s|v$currentVersion|v$newVersion|g" ../setup.sh
+sed -i "s|v$currentVersion|v$newVersion|g" ./version
