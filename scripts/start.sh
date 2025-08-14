b7dbf1698be11dd06cbbfe8d5d16747e4026d890
WOOSAH <31331596+Smiley-McSmiles@users.noreply.github.com>
2025-08-13 20:12:57 -0500

    Add files via upload

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-configFile=/opt/open-webui/config/llaman.conf
+defaultDir=/opt/open-webui
+configFile=$defaultDir/config/llaman.conf
 source $configFile
 
-defaultDir=/opt/open-webui
 condaSh=$defaultDir/miniconda3/etc/profile.d/conda.sh
 condaEnv=$defaultDir/config/conda/open-webui
 openWebuiStart="open-webui serve --port $httpPort"
